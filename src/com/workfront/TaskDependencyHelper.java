4b3204afc72c6be0354169982bf54031f29b3c21
Joe Wiggins <joewiggins@attask.com>
2015-05-11 17:17:35 -0600

    Updating description for TaskDependencyHelper to match solution

--- src/com/workfront/TaskDependencyHelper.java
+++ src/com/workfront/TaskDependencyHelper.java
@@ -4,21 +4,15 @@ public class TaskDependencyHelper {
 
 	/**
 	 * The purpose of this class is to ensure that certain types of tasks that are entered follow the proper
-	 * process to facilitate consistent reporting.  When a task is entered under a certain project and is
-	 * marked in custom data as an "Agile Story," we want to ensure that the story has a certain set of subtasks.
+	 * process to facilitate consistent reporting.  When a task is entered under the Development Product Backlog
+	 * project and is and has the form "Story" associated with it, we want to ensure that the story has a certain
+	 * set of subtasks.
 	 *
 	 * So to recap, the requirements are:
 	 *
-	 * In Workfront:
-	 * 1. Add a custom field called "Agile Story" that is a checkbox to a custom form of your choosing that will
-	 *    be applied to agile team stories.
-	 *
-	 * In this class:
-	 * 2. When a task on the Development Product Backlog has the custom field "Agile Story" selected, ensure the following
-	 *    subtasks exist:
-	 *    	"Technical Approach": 4 hours default duration and assigned to the team
-	 *      "UX Design": 2 days default duration and assigned to team
-	 *      "QA Test Map": 2 days default duration and assigned to team
+	 * 1. When a task on the Development Product Backlog has the custom form "Story" selected and is assigned
+	 *    to an agile team, ensure the following subtasks exist and are assigned to the same team as the story:
+	 *      "Technical Approach", "UX Design", "QA Test Map"
 	 *
 	 * @param args
 	 */
